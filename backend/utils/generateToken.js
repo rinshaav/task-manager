f444aafac9c10cd25f218e00aac5ff32fa02649b
rinshaav <rinshaavpoovathikkal@gmail.com>
2026-09-06 15:31:05 +0530

    Fix authentication cookie for production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,13 @@ const generateToken = (res, userId) => {
   const token = jwt.sign(
     { userId },
     process.env.JWT_SECRET,
-    {
-      expiresIn: "7d",
-    }
+    { expiresIn: "7d" }
   );
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
